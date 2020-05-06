c5e94c7dfdfb53dddf7501c86abb19b0431184dc
SujinChoi <alluere1212.c@gmail.com>
2020-05-06 17:06:32 +0900

    0506 17:06 sujin

--- jy/src/main/java/com/mj/jy/commute/controller/CommuteController.java
+++ jy/src/main/java/com/mj/jy/commute/controller/CommuteController.java
@@ -2,11 +2,15 @@ package com.mj.jy.commute.controller;
 
 import java.util.ArrayList;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.google.gson.Gson;
 import com.mj.jy.commute.model.service.CommuteService;
 import com.mj.jy.commute.model.vo.CommuteDto;
 
@@ -24,4 +28,6 @@ public class CommuteController {
 		model.addAttribute("list", list);
 		return "commute/commuteListView";
 	}
+	
+
 }
