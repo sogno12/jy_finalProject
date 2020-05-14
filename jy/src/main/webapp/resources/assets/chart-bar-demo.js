a9af14233134d26ecb9ba135e022e23c8cf8d1ea
minjin1213 <62366330+bbc719@users.noreply.github.com>
2020-05-14 14:03:38 +0900

    commuteChart

--- jy/src/main/webapp/resources/assets/chart-bar-demo.js
+++ jy/src/main/webapp/resources/assets/chart-bar-demo.js
@@ -4,15 +4,25 @@ Chart.defaults.global.defaultFontColor = '#292b2c';
 
 // Bar Chart Example
 var ctx = document.getElementById("myBarChart");
+
+$.ajax({
+	
+})
 var myLineChart = new Chart(ctx, {
+	
+
   type: 'bar',
   data: {
-    labels: ["January", "February", "March", "April", "May", "June"],
+    labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
     datasets: [{
       label: "Revenue",
-      backgroundColor: "rgba(2,117,216,1)",
-      borderColor: "rgba(2,117,216,1)",
-      data: [4215, 5312, 6251, 7841, 9821, 14984],
+      backgroundColor: "#28a745",
+      borderColor: "#28a745",
+      data: [4215, 5312, 6251, 7841, 9821, 14984, 14984, 14984, 14984, 14984, 14984, 14984]},
+      {label: "Revenue",
+    	  backgroundColor: "#ffc107",
+          borderColor: "#ffc107",
+      data: [4215, 5312, 6251, 7841, 9821, 14984, 14984, 14984, 14984, 14984, 14984, 14984]
     }],
   },
   options: {
@@ -25,7 +35,7 @@ var myLineChart = new Chart(ctx, {
           display: false
         },
         ticks: {
-          maxTicksLimit: 6
+          maxTicksLimit: 13
         }
       }],
       yAxes: [{
